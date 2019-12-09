2b7acfbef231b661e73ded0c895daa0cb030e9ae
Maxime <Maxime@DESKTOP-JS4CBKA>
2019-12-09 21:26:43 +0100

    Noms des joueurs entré et centré

--- src/application/direct1Controller.java
+++ src/application/direct1Controller.java
@@ -4,17 +4,73 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javafx.fxml.FXML;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class direct1Controller {
 	
 	@FXML
+	private Text r11, r12, r13, r14, r15, r16, r17;
+	@FXML
+	private Text r21, r22, r23, r24, r25, r26, r27;
+	@FXML
+	private Text t11, t12, t13, t14, t15, t16, t17;
+	@FXML
+	private Text t21, t22, t23, t24, t25, t26, t27;
+	@FXML
+	private AnchorPane rempEqu1, rempEqu2, tituEqu1, tituEqu2;
+	
+	int i = 0;
+	
+	@FXML
 	public void initialize() {
-		System.out.println("cccccccezoirezopri");
+		// Liste joueurs equipe 1
+		Joueur titu11 = new Gardien("SEGO", "Marin", "Montpellier", "#1");
+		Joueur remp11 = new Gardien("BONNEFOI", "Kevin", "Montpellier", "#12");
+		Joueur titu12 = new JoueurChamp("AFGOUR", "Benjamin", "Montpellier", "#33");
+		Joueur titu13 = new JoueurChamp("BOS", "Julien", "Montpellier", "#13");
+		Joueur titu14 = new JoueurChamp("DUARTE", "Gilberto", "Montpellier", "#90");
+		Joueur titu15 = new JoueurChamp("GUIRAUDOU", "Paul Louis", "Montpellier", "#2");
+		Joueur titu16 = new JoueurChamp("LENNE", "Yanis", "Montpellier", "#32");
+		Joueur titu17 = new JoueurChamp("MENGON", "Marco", "Montpellier", "#15");
+		Joueur remp12 = new JoueurChamp("PETTERSSON", "Frederic", "Montpellier", "#18");
+		Joueur remp13 = new JoueurChamp("PORTE", "Valentin", "Montpellier", "#28");
+		Joueur remp14 = new JoueurChamp("RICHARDSON", "Melvyn", "Montpellier", "#22");
+		Joueur remp15 = new JoueurChamp("SIMONET", "Diego", "Montpellier", "#4");
+		Joueur remp16 = new JoueurChamp("SOUSSI", "Mohammed", "Montpellier", "#39");
+		Joueur remp17 = new JoueurChamp("TRUCHANOVICIUS", "Jonas", "Montpellier", "#7");
+		
+		// Liste joueurs equipe 2
+		Joueur titu21 = new Gardien("LETTENS", "Jef", "Toulouse", "#1");
+		Joueur remp21 = new Gardien("GEHIN", "Theo", "Toulouse", "#12");
+		Joueur titu22 = new JoueurChamp("LEVENTOUX", "Remi", "Toulouse", "#2");
+		Joueur titu23 = new JoueurChamp("CHELLE", "Pierrick", "Toulouse", "#3");
+		Joueur titu24 = new JoueurChamp("OLSSON", "Markus", "Toulouse", "#4");
+		Joueur titu25 = new JoueurChamp("GARCIA", "Arnau", "Toulouse", "#9");
+		Joueur titu26 = new JoueurChamp("GILBERT", "Maxime", "Toulouse", "#10");
+		Joueur titu27 = new JoueurChamp("SOLE", "Ferran", "Toulouse", "#14");
+		Joueur remp22 = new JoueurChamp("ILIC", "Nemanja", "Toulouse", "#19");
+		Joueur remp23 = new JoueurChamp("STEINS", "Luc", "Toulouse", "#22");
+		Joueur remp24 = new JoueurChamp("TRIBILLON", "Gael", "Toulouse", "#33");
+		Joueur remp25 = new JoueurChamp("BONILAURI", "Jordan", "Toulouse", "#47");
+		Joueur remp26 = new JoueurChamp("ABDI", "Ayoub", "Toulouse", "#87");
+		Joueur remp27 = new JoueurChamp("GIRAUDEAU", "Romain", "Toulouse", "#11");
+		
+		Text[] tituText = {t11, t12, t13, t14, t15, t16, t17, t21, t22, t23, t24, t25, t26, t27};
+		Text[] rempText = {r11, r12, r13, r14, r15, r16, r17, r21, r22, r23, r24, r25, r26, r27};
+		Joueur[] tituJoueur = {titu11, titu12, titu13, titu14, titu15, titu16, titu17, titu21, titu22, titu23, titu24, titu25, titu26, titu27};
+		Joueur[] rempJoueur = {remp11, remp12, remp13, remp14, remp15, remp16, remp17, remp21, remp22, remp23, remp24, remp25, remp26, remp27};
+		
+		for(i = 0; i < 14; i++) {
+			tituText[i].setText(tituJoueur[i].getPrenom().substring(0, 1) + ". " + tituJoueur[i].getNom());
+			rempText[i].setText(rempJoueur[i].getPrenom().substring(0, 1) + ". " + rempJoueur[i].getNom());
+		}
+		
 	}
 	
 
